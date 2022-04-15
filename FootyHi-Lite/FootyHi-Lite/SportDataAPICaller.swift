b045f42b0736610830b26ee7885d3bb3e040d44c
Nick Haider <79616565+NickHaider@users.noreply.github.com>
2022-04-15 19:48:15 -0400

    Apr15-Latest Status

---
+++ FootyHi-Lite/FootyHi-Lite/SportDataAPICaller.swift
@@ -0,0 +1,16 @@
+//
+//  SportDataAPICaller.swift
+//  FootyHi-Lite
+//
+//  Created by Nick on 2022-04-15.
+//
+
+import UIKit
+import BDBOAuth1Manager
+
+class TwitterAPICaller: BDBOAuth1SessionManager {
+    
+    
+    
+    
+}
