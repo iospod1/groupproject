6487e880a7883a41c8c2b2638509a43b00c08709
Yang Li <yal319@ucsd.edu>
2022-04-15 19:07:19 -0400

    Initialize pod

---
+++ FootyHi-Lite/Pods/Target Support Files/Pods-FootyHi-Lite-FootyHi-LiteUITests/Pods-FootyHi-Lite-FootyHi-LiteUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FootyHi_Lite_FootyHi_LiteUITests : NSObject
+@end
+@implementation PodsDummy_Pods_FootyHi_Lite_FootyHi_LiteUITests
+@end
---
+++ FootyHi-Lite/Pods/Target Support Files/Pods-FootyHi-Lite/Pods-FootyHi-Lite-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FootyHi_Lite : NSObject
+@end
+@implementation PodsDummy_Pods_FootyHi_Lite
+@end
