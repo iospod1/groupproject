643a070aae9c2cddc7fb013b3011074ab9f977aa
gyaltsentenzin <40735388+gyaltsentenzin@users.noreply.github.com>
2022-04-23 00:20:21 -0500

    Final Version

--- FootyHi-Lite/FootyHi-Lite/BetCell.swift
+++ FootyHi-Lite/FootyHi-Lite/BetCell.swift
@@ -9,9 +9,10 @@ import UIKit
 
 class BetCell: UITableViewCell {
     
-    @IBOutlet weak var topWinners: UILabel!
-    @IBOutlet weak var teamName: UILabel!
-    @IBOutlet weak var teamImage: UIImageView!
+    @IBOutlet weak var homeLogo: UIImageView!
+    @IBOutlet weak var homeTeam: UILabel!
+    @IBOutlet weak var awayLogo: UIImageView!
+    @IBOutlet weak var awayTeam: UILabel!
     
     override func awakeFromNib() {
         super.awakeFromNib()
--- FootyHi-Lite/FootyHi-Lite/BetViewController.swift
+++ FootyHi-Lite/FootyHi-Lite/BetViewController.swift
@@ -11,28 +11,61 @@ import Parse
 class BetViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     @IBOutlet weak var tableView: UITableView!
-    
+    var matches = [[String: AnyObject]]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         tableView.delegate = self
         tableView.dataSource = self
+        
+        let url = URL(string: "https://app.sportdataapi.com/api/v1/soccer/matches?apikey=\(API_KEY)&season_id=1980&date_from=2022-04-25")!
+        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        let task = session.dataTask(with: request) { (data, response, error) in
+             // This will run when the network request returns
+             if let error = error {
+                     print(error.localizedDescription)
+             } else if let data = data {
+                    let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: AnyObject]
+
+                 print(dataDictionary)
+                 self.matches =  dataDictionary["data"] as! [[String: AnyObject]]
+                 self.tableView.reloadData()
+
+             }
+        }
+        task.resume()
 
         // Do any additional setup after loading the view.
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return matches.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "BetCell", for: indexPath) as! BetCell
         
-        cell.topWinners.text = "Top Winners"
-        cell.teamName.text = "Arsenal FC"
-        cell.teamImage.image = UIImage(named: "Players")
+        let currentMatch = matches[indexPath.row]
         
+        let home = currentMatch["home_team"] as! [String:AnyObject]
+        let homeTeam = home["name"] as! String
+        let homeImageURL = URL(string: (home["logo"] as? String)!)
+        let homeImage = try? Data(contentsOf: homeImageURL!)
+        
+        let away = currentMatch["away_team"] as! [String:AnyObject]
+        let awayTeam = away["name"] as! String
+        let awayImageURL = URL(string: (away["logo"] as? String)!)
+        let awayImage = try? Data(contentsOf: awayImageURL!)
+        
+        
+        cell.homeTeam.text = homeTeam
+        cell.homeLogo.image = UIImage(data: homeImage!)
+        
+        cell.awayTeam.text = awayTeam
+        cell.awayLogo.image = UIImage(data: awayImage!)
+                
         return cell
     }
     
